ca9d6fc50fb474fbe1e7fb84f12da3536ff7bedb
glennerad <>
2018-10-30 15:09:23 +0300

    *important* Fix error in case of not found segments

--- ch_scripts/parse_tcr/igblast/parse_tcr.py
+++ ch_scripts/parse_tcr/igblast/parse_tcr.py
@@ -8,7 +8,8 @@ from Bio import SeqIO
 #==============================
 curr_version = 1.0
 parser = argparse.ArgumentParser(
-    description="This script will find CDR positions in TCR sequences.")
+    description="This script will find CDR positions in TCR sequences. "
+                "Write IGDATA path to igblast , if you run it from different directory")
 
 parser.add_argument(
     "--inpfile", nargs=1, type=str, default="../test_tcr_human.txt",#_nucleotide.fasta",
@@ -21,6 +22,9 @@ parser.add_argument(
 parser.add_argument(
     "--specie", nargs=1, type=str, default="HomoSapiens", help="Specie name in CamelCase")
 
+
+igblast_db_path = os.path.dirname(os.path.realpath(__file__))
+print("Running parse_tcr.py in {}\n".format(igblast_db_path))
 args = parser.parse_args()
 
 if type(args.inpfile) is list:
@@ -48,7 +52,7 @@ tmpfile_j = ".".join(inpfile.split(".")[:-1])+"_j_blasted.txt"
 
 segments = ("Variable", "Diversity", "Joining")
 
-workdir = os.path.join("igblast_db", specie.lower(), "{}_{}_{}_sequences.fasta".format(seq_type, specie, "{}"))
+workdir = os.path.join(igblast_db_path, "igblast_db", specie.lower(), "{}_{}_{}_sequences.fasta".format(seq_type, specie, "{}"))
 
 v_db = workdir.format("Variable")
 d_db = workdir.format("Diversity")
@@ -69,9 +73,13 @@ else:
 
 blastcmd = "{} -db {} -query {} -out {}".format(blastcmd, j_db, inpfile, tmpfile_j)
 igblastcmd = "{} -germline_db_V {} {}-query {} -out {}".format(igblastcmd, v_db, cmd, inpfile, tmpfile_v)
-os.system(blastcmd)
-os.system(igblastcmd)
 
+print("Running BLAST:\n{}".format(blastcmd))
+os.system(blastcmd)
+print("Done\n")
+print("Running IgBLAST:\n{}".format(igblastcmd))
+os.system(igblastcmd)
+print("Done\n")
 
 segment_dict = {"FR1-IMGT": "FR1", "FR2-IMGT": "FR2", "CDR1-IMGT": "CDR1", "CDR2-IMGT":"CDR2"}
 outinfo = []
@@ -86,19 +94,22 @@ with open(tmpfile_v, "r") as inp:
                 query += line.strip()
 
             for line in inp:
+                if line.startswith("Sequences producing significant alignments: "):
+                    inp.readline()
+                    v = inp.readline().strip().split()[0]
                 if line.startswith(("FR1", "CDR1", "FR2", "CDR2", "FR3")):
                     info = line.strip().split()
                     start_pos = int(info[1])
                     end_pos = int(info[2])
                     if line.startswith("FR3"):
-                        outinfo.append([query, "FR3", start_pos - 1, end_pos-1])
-                        outinfo.append([query, "CDR3", end_pos - 1, 0])
+                        outinfo.append([query, "FR3", start_pos - 1, end_pos-1, v, ""])
+                        outinfo.append([query, "CDR3", end_pos - 1, 0, v, ""])
                         break
                     elif info[0] in segment_dict:
-                        outinfo.append([query, segment_dict[info[0]], start_pos - 1, end_pos])
+                        outinfo.append([query, segment_dict[info[0]], start_pos - 1, end_pos, v, ""])
 
 
-outdf = pd.DataFrame(outinfo, columns=["query", "CDRtype", "start", "end"])
+outdf = pd.DataFrame(outinfo, columns=["query", "region", "start", "end", "v.gene", "j.gene"])
 
 with open(tmpfile_j, "r") as inp:
     for line in inp:
@@ -109,12 +120,19 @@ with open(tmpfile_j, "r") as inp:
                     break
                 query += line.strip()
             for line in inp:
+                if line.startswith("***** No hits found *****"):
+                    outdf.loc[(outdf["query"] == query) & (outdf["region"] == "CDR3"), "end"] = -1
+                    outdf.loc[(outdf["query"] == query), "j.gene"] = -1
+                    break
+                if line.startswith(">"):
+                    j = line.strip().split()[0].replace(">", "")
+                    outdf.loc[(outdf["query"] == query), "j.gene"] = j
                 if line.startswith("Query "):
                     info = line.split()
-                    outdf.loc[(outdf["query"] == query) & (outdf["CDRtype"] == "CDR3"), "end"] = int(info[1])
+                    outdf.loc[(outdf["query"] == query) & (outdf["region"] == "CDR3"), "end"] = int(info[1])
                     break
 
-outdf["length"] = outdf["end"] - outdf["start"]
+#outdf["end"] - outdf["start"]
 
 
 def slice_sequence(df, fullseqcol, seqcol, startcol, endcol):
@@ -131,9 +149,10 @@ with open(inpfile, "r") as inp:
 
 
 outdf["sequence"] = slice_sequence(outdf, "fullseq", "sequence", "start", "end")
+outdf["length"] = outdf["sequence"].str.len()
 
-outdf.to_csv(outfile, columns=["query", "CDRtype", "start", "end", "length", "sequence"], sep="\t", index=None)
+outdf.to_csv(outfile, columns=["query", "region", "start", "end", "length", "sequence", "v.gene", "j.gene"], sep="\t", index=None)
 
 
-os.remove(tmpfile_j)
-os.remove(tmpfile_v)
+#os.remove(tmpfile_j)
+#os.remove(tmpfile_v)
